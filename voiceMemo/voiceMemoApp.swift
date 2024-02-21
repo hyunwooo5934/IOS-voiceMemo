bcdb9f0a51f465ef745affc705418d85bee39872
HyunwooKim1 <hwkim@tecace.com>
2024-02-21 09:24:43 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by tecace on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
